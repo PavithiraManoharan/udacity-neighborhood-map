4d05044e30f1b77bb095f7433b4ccd7aa9b10c40
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2019-01-21 13:15:00 +0100

    Added Google Maps without any React Component

--- src/App.js
+++ src/App.js
@@ -3,26 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyABkyhI9EIbhjjP5D-mQTQo-4l-9h34Nm4&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 53.55, lng: 10},
+      zoom: 14
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
 }
 
+/**
+ * Create a function called loadScript to simulate adding a script tag in the index.html page
+ * <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+ */
+
+ function loadScript(url) {
+    let index = window.document.getElementsByTagName("script")[0]
+    let script = window.document.createElement("script")
+    script.src = url
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
+ }
+ 
 export default App;
