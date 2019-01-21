312148056a8ecc8981b1ba7bdf7b9c75e9e546f9
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2019-01-21 18:29:53 +0100

    Added stylish markers with animation and useful info

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     const parameters = {
       client_id: "I35VHQNE1A5YIW0SCEGXNVAQ4UOVGEORZV5X5V2ABI4PUSCF",
       client_secret:   "TS53QTLYEOVAGUZUPF4EEFZGCMGUZLVJ2G4OPMPWMP5UDPQB",
-      query: "museum",
+      query: "poi",
       section: "museum",
       categoryId: "4bf58dd8d48988d181941735",
       near:"Hafencity",
@@ -46,35 +46,80 @@ class App extends Component {
       })
   } 
 
+
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  makeMarkerIcon = (markerColor) => {
+    var markerImage = new window.google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new window.google.maps.Size(21, 34),
+      new window.google.maps.Point(0, 0),
+      new window.google.maps.Point(10, 34),
+      new window.google.maps.Size(21,34));
+    return markerImage;
+  }
+
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 53.55, lng: 10},
-      zoom: 14
+      zoom: 14,
     });
+
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = this.makeMarkerIcon('ff0000');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = this.makeMarkerIcon('0091ff');
 
     //Create an Infowindow
     let infowindow = new window.google.maps.InfoWindow()
     
     // Display dynamic markers
     this.state.venues && this.state.venues.map(currentPlace => {
-      let contentString = `${currentPlace.venue.name}`
-     
+      let contentString = `
+      <h3 id="placeName">${currentPlace.venue.name}</h4>
+      <p><strong>Where? </strong>${currentPlace.venue.location.formattedAddress.map(addressLine => {
+        return ` ${addressLine}`
+      })}</p>
+      <p><strong>Category: </strong>${currentPlace.venue.categories[0].name}</p>
+      `
       //Create a marker
       let marker = new window.google.maps.Marker({
         position: {lat: currentPlace.venue.location.lat, lng: currentPlace.venue.location.lng},
         map: map,
+        animation: window.google.maps.Animation.DROP,
         title: currentPlace.venue.name
       })
+
+      marker.setIcon(defaultIcon)
       
       marker.addListener('click', function() {
         //Set content of the infowindow
         infowindow.setContent(contentString)
         //Open it
         infowindow.open(map, marker);
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE);
+          setTimeout(function(){ marker.setAnimation(null); }, 1300);
+        }
+      });
+
+      // Two event listeners - one for mouseover, one for mouseout,
+      // to change the colors back and forth.
+      marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon);
+      });
+      marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
       });
 
       return marker
-    })
+    }) 
   }
 
   render() {
