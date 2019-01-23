71a04fb98edfe081091e78ddb333f97eaad58124
Pavithira Manoharan <pavithira.manoharan@gmail.com>
2019-01-23 12:19:40 +0100

    Filtered venues and the corresponding List items!

--- src/FilterSection.js
+++ src/FilterSection.js
@@ -7,26 +7,33 @@ class FilterSection extends Component {
     constructor(props){
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            filteredVenues: []
         }
     }
 
+    componentDidMount() {
+        this.setState({filteredVenues: this.props.venues})
+    }
+
     filterVenues(query) {
+        let filteredVenues = (this.props.venues).filter(eachPlace => eachPlace.venue.name.toLowerCase().includes(query.toLowerCase()))
         this.props.markers.forEach(marker => {
             marker.name.toLowerCase().includes(query.toLowerCase()) === true ? 
                 marker.setVisible(true) :
                 marker.setVisible(false)
         })
         this.setState({ query })
+        this.setState({ filteredVenues })
     }
  
     render() {
         return( 
             <div className="sidebar">
-                <input value= {this.state.query} onChange={(e) => { this.filterVenues(e.target.value) }}/>
+                <input placeorder="Filter content" value= {this.state.query} onChange={(e) => { this.filterVenues(e.target.value) }}/>
                 <ol>
-                    {this.props.venues && (this.props.venues).map((currentLocation) =>
-                            <li key={currentLocation.venue.id}>{currentLocation.venue.name}</li>
+                    {this.state.filteredVenues && (this.state.filteredVenues).map((currentLocation, index) =>
+                        <li key={index}>{currentLocation.venue.name}</li>
                     )}
                 </ol>        
             </div>
